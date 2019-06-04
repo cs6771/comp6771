6b625e981db562eab43e7fc0c84fa726d7bbc4bd
Matt Stark <mattstark75@gmail.com>
2019-06-04 15:13:53 +1000

    Add data directory and use it for week 2 tutorial.

--- tutorials/week2/dictionary.cpp
+++ tutorials/week2/dictionary.cpp
@@ -6,6 +6,6 @@
 #include "tutorials/week2/dictionary_library.h"
 
 int main() {
-  std::ifstream word_list_file = std::ifstream{"/usr/share/dict/words"};
+  std::ifstream word_list_file = std::ifstream{"data/words.txt"};
   print_valid_words(to_word_list(word_list_file), std::cin, std::cout);
 }
