266ce9f5b3939185ea70e2a27942556b1d7a698f
chamhayden <mail@chambalam.com>
2019-06-06 19:37:55 +1000

    Consolidating all assignmnet files in 1 place (#8)
    * Consolidating all assignmnet files in 1 place
    
    * Word ladder test top comment

--- assignments/wl/word_ladder_test.cpp
+++ assignments/wl/word_ladder_test.cpp
@@ -1,3 +1,13 @@
+/*
+
+  == Explanation and rational of testing ==
+
+  Explain and justify how you approached testing, the degree
+   to which you're certain you have covered all possibilities,
+   and why you think your tests are that thorough.
+
+*/
+
 #include "assignments/wl/word_ladder.h"
 #include "catch.h"
 
