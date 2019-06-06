552e3c198b9212aa36a33b5a702659b7996648d2
Hayden Smith <mail@chambalam.com>
2019-06-06 19:11:42 +1000

    Consolidating all assignmnet files in 1 place

--- assignments/wl/main.cpp
+++ assignments/wl/main.cpp
@@ -4,7 +4,7 @@
 #include "assignments/wl/word_ladder.h"
 
 int main() {
-  auto lexicon = GetLexicon("data/words.txt");
+  auto lexicon = GetLexicon("assignments/wl/words.txt");
 
   // TODO(students): Replace this with your code
   for (const auto& word : lexicon) {
