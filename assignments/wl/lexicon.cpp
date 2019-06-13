8f4ab45aa5a6676d41c998c3d07685be8616b770
matt <mattstark75@gmail.com>
2019-06-13 22:06:35 +1000

    Replaced word ladder with unordered set.

--- assignments/wl/lexicon.cpp
+++ assignments/wl/lexicon.cpp
@@ -5,19 +5,19 @@
 #include <fstream>
 #include <iostream>
 #include <iterator>
-#include <set>
+#include <unordered_set>
 
 void Error(const std::string& message) {
   std::cout << message << std::endl;
   std::exit(1);
 }
 
-std::set<std::string> GetLexicon(const std::string& filename) {
+std::unordered_set<std::string> GetLexicon(const std::string& filename) {
   std::ifstream f{filename};
   if (!f) {
     Error("Failed to open file");
   }
-  std::set<std::string> lexicon;
+  std::unordered_set<std::string> lexicon;
   std::copy(std::istream_iterator<std::string>(f), {},
             std::inserter(lexicon, lexicon.end()));
   if (f.bad()) {
--- assignments/wl/lexicon.h
+++ assignments/wl/lexicon.h
@@ -1,12 +1,12 @@
 #ifndef ASSIGNMENTS_WL_LEXICON_H_
 #define ASSIGNMENTS_WL_LEXICON_H_
 
-#include <set>
+#include <unordered_set>
 #include <string>
 
 // Later on in semester we will learn about exceptions. But for now, we just exit on failure.
 void Error(const std::string& message);
 
-std::set<std::string> GetLexicon(const std::string& filename);
+std::unordered_set<std::string> GetLexicon(const std::string& filename);
 
 #endif  // ASSIGNMENTS_WL_LEXICON_H_
