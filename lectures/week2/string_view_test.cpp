72b39c927fc1645d361f06303ab8245c96ba5506
matt <mattstark75@gmail.com>
2019-06-14 03:02:12 +1000

    Add a predicate to the demo.

--- lectures/week2/string_view_test.cpp
+++ lectures/week2/string_view_test.cpp
@@ -1,6 +1,13 @@
 #include "catch.h"
 #include "lectures/week2/string_view.h"
 
+// Function definitions can have auto return types in C++17
+auto EqualsString([[maybe_unused]] const std::vector<std::string>& strings) {
+  return Catch::Predicate<RopeView>([] ([[maybe_unused]] const RopeView expected) {
+    return true;
+  });
+}
+
 SCENARIO() {
   WHEN("You try to split an empty string") {
     std::string emptyString;
@@ -8,6 +15,8 @@ SCENARIO() {
     THEN("You get a single empty result") {
       CHECK(result.size() == 1);
       REQUIRE(std::distance(result[0].first, result[0].second) == 0);
+      // Alternatively, we can use predicates.
+      REQUIRE_THAT(result, EqualsString({""}));
     }
   }
 }
