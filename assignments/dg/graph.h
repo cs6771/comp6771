9896e1e15f914b201efea9f60a6c6db6b88c9c75
Hayden Smith <mail@chambalam.com>
2019-07-15 00:36:49 +1000

    cpplint and other fixes

--- assignments/dg/graph.h
+++ assignments/dg/graph.h
@@ -3,16 +3,14 @@
 
 namespace gdwg {
 
-  template<typename N, typename E>
-  class Graph {
-   public:
-
-   private:
-
-  };
-
-  #include "assignments/dg/graph.inc"
+template<typename N, typename E>
+class Graph {
+ public:
+ private:
+};
 
 }
 
-#endif // ASSIGNMENTS_DG_GRAPH_H_
+#include "assignments/dg/graph.inc"
+
+#endif  // ASSIGNMENTS_DG_GRAPH_H_
