f191a7fda42022d258a41204939f7480b4b27e6e
Hayden Smith <mail@chambalam.com>
2019-07-15 23:22:07 +1000

    Week 7 code

---
+++ lectures/week7/static.h
@@ -0,0 +1,29 @@
+#ifndef LECTURES_WEEK7_STACK_H_
+#define LECTURES_WEEK7_STACK_H_
+
+#include <vector>
+
+template <typename T>
+class Stack {
+ public:
+  Stack();
+  ~Stack();
+  void push(T&);
+  void pop();
+  T& top();
+  const T& top() const;
+  static int numStacks_;
+ private:
+  std::vector<T> stack_;
+};
+
+template <typename T>
+int Stack<T>::numStacks_ = 0;
+
+template <typename T>
+Stack<T>::Stack() { numStacks_++; }
+
+template <typename T>
+Stack<T>:: ~Stack() { numStacks_--; }
+
+#endif  // LECTURES_WEEK7_STACK_H_
