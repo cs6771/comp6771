ed19e309bcf90e2f9cd6851627e3eff604d80bff
chamhayden <mail@chambalam.com>
2019-07-17 04:00:44 +1000

    Week 7 code (#40)
    * Week 7 code
    
    * Matt fixes

---
+++ lectures/week7/static_use.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include "lectures/week7/static.h"
+
+int main() {
+  Stack<float> fs;
+  Stack<int> is1, is2, is3;
+  std::cout << Stack<float>::numStacks_ << "\n";
+  std::cout << Stack<int>::numStacks_ << "\n";
+}
