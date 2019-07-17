b2b49d3f18fff2563e05acf693e723fa4d8783a4
matt <mattstark75@gmail.com>
2019-07-17 14:30:31 +1000

    Add iterator demo to starter code for graph assignment.

--- assignments/dg/client.cpp
+++ assignments/dg/client.cpp
@@ -1,8 +1,10 @@
-// Note: At the moment, there is no client.sampleout. Please do your own testing
-
+#include <iostream>
 #include <string>
 
 #include "assignments/dg/graph.h"
+
+// Note: At the moment, there is no client.sampleout. Please do your own testing
+
 
 int main() {
   gdwg::Graph<std::string, int> g;
@@ -20,9 +22,16 @@ int main() {
 
   g.InsertEdge("are", "you?", 3);
 
-  std::cout << g << std::endl;
+  std::cout << g << '\n';
 
-  gdwg::Graph<std::string, int> g2 = g;
+  gdwg::Graph<std::string, int> g2{g};
   
   std::cout << g2 << "\n";
+
+  // This is a structured binding.
+  // https://en.cppreference.com/w/cpp/language/structured_binding
+  // It allows you to unpack your tuple.
+  for (const auto& [from, to, weight] : g) {
+    std::cout << from << " -> " << to << " (weight " << weight << ")\n";
+  }
 }
