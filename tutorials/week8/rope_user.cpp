960b0e7e485f9826298a4bd86afabb4a47a4932b
matt <mattstark75@gmail.com>
2019-07-22 00:46:28 +1000

    Add better tests for students.

--- tutorials/week8/rope_user.cpp
+++ tutorials/week8/rope_user.cpp
@@ -3,9 +3,23 @@
 #include "tutorials/week8/rope.h"
 
 int main() {
-  Rope rope{{"hello", " ", "world"}};
+  std::vector<Rope> ropes{
+      Rope{{"a"}},
+      Rope{{"abc"}},
+      Rope{{"abc"}},
+      Rope{{"abc", "def"}},
+      Rope{{"abc", "", "def"}},
+      Rope{{""}},
+      Rope{{"", "abc", "def", ""}}
+  };
 
-  for (char letter : rope) {
-    std::cout << letter << '\n';
+  // TODO(tutorial): make this support const auto& rope, if you have time.
+  for (auto& rope : ropes) {
+    std::cout << "Rope: \"";
+    for (char letter : rope) {
+      std::cout << letter;
+    }
+    std::cout << "\"\n";
   }
 }
+
